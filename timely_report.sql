d45bea424a3c7cf50f57457135745965a7a0f370
Benjamin Fauchald <benjaminfauchald@Benjamins-Mac-mini.local>
2023-02-08 17:26:42 +0700

    timley reports v0

---
+++ timely_report.sql
@@ -0,0 +1,54 @@
+
+select
+         CAST(DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY) as STRING) as date,
+        u.id,
+        first_name,
+        last_name,
+
+        -- Total Hours
+        COALESCE((select sum(hours) from sps-business-insight.sps_raw_harvest.time_entries te
+        where
+            user_id=u.id
+            and
+            (
+                CAST(FORMAT_DATETIME('%F', te.spent_date) as STRING) = CAST(DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY) as STRING)       )
+            ),0) as total_hours,
+
+        -- Total billable hours
+        COALESCE((select sum(hours) from sps-business-insight.sps_raw_harvest.time_entries te
+        where
+            user_id=u.id
+            and te.billable = true
+            and
+            (
+                CAST(FORMAT_DATETIME('%F', te.spent_date) as STRING) = CAST(DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY) as STRING)       )
+            ),0) as billabe_hours,
+
+        -- Total unbillable hours
+        COALESCE((select sum(hours) from sps-business-insight.sps_raw_harvest.time_entries te
+        where
+            user_id=u.id
+            and te.billable = false
+            and
+            (
+                CAST(FORMAT_DATETIME('%F', te.spent_date) as STRING) = CAST(DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY) as STRING)       )
+            ),0) as unbillable_hours,
+
+
+
+            -- Total timely logged hours
+        (select sum(hours) from sps-business-insight.sps_raw_harvest.time_entries te
+        where
+            user_id=u.id
+            and CAST(FORMAT_DATETIME('%F', te.spent_date) as STRING) = CAST(DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY) as STRING)
+            and CAST(FORMAT_DATETIME('%F', te.spent_date) as STRING) =CAST(FORMAT_DATETIME('%F', te.created_at) as STRING)
+        ) as timely_logged
+
+
+
+from
+    sps-business-insight.sps_raw_harvest.users u
+where
+    u.is_active = true
+order by
+    u.id
